08fbc9ddfdd0958bb89c5591de9699320e6f8d6d
m.kurahara <kuraharamasahiro@gmail.com>
2020-08-11 15:23:40 +0900

    addon-notes 実装

--- src/stories/shared.stories.ts
+++ src/stories/shared.stories.ts
@@ -1,14 +1,27 @@
 import { ButtonComponent } from '../app/shared/component/button/button.component';
 import { TextComponent } from '../app/shared/component/text/text.component';
+import { withNotes } from '@storybook/addon-notes'
+import buttonNote from './notes/button.md';
 
 export default {
   title: 'App Component',
+  parameters: {
+    notes: 'some documentation herefaisdjafijisfjai',
+  },
 };
 
 export const button = () => ({
   component: ButtonComponent,
   props: {},
 });
+
+button.story = {
+  parameters: {
+    notes: {Introduction: buttonNote},
+  }
+}
+
+
 
 export const text = () => ({
   component: TextComponent,
