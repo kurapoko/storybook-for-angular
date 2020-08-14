95ce3fd380f7db0fcb86b7aa4cda2ffb162dbc86
m.kurahara <kuraharamasahiro@gmail.com>
2020-08-14 12:59:54 +0900

    モック作成

--- src/stories/shared.stories.ts
+++ src/stories/shared.stories.ts
@@ -1,6 +1,7 @@
 import { ButtonComponent } from '../app/shared/component/button/button.component';
 import { TextComponent } from '../app/shared/component/text/text.component';
-import { withNotes } from '@storybook/addon-notes'
+import { moduleMetadata, storiesOf } from '@storybook/angular';
+import { withNotes } from '@storybook/addon-notes';
 import buttonNote from './notes/button.md';
 
 export default {
@@ -30,4 +31,15 @@ export const text = () => ({
 
 export const template = () => ({
   template: '<p>template</p>',
+});
+
+export const htmlTag = () => ({
+  template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
+  props: {
+    text: 'Hello Button',
+    onClick: (event: Event) => {
+      console.log('some bindings work');
+      console.log(event);
+    },
+  },
 });
